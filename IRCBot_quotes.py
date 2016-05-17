4a752e00376a49c16d9b9da2f32f8c87933f10df
Thompson Lee <tom.mai78101@gmail.com>
2016-05-17 00:22:15 -0400

    Fixed typo.

--- IRCBot_quotes.py
+++ IRCBot_quotes.py
@@ -20,7 +20,7 @@ class QuotesBot(UpdateBot):
 						tempString = ""
 						for i in range(2, len(messageTokens)):
 							tempString += messageTokens[i] + " "
-						self.wedrs.append("[%s] %s" % (user, tempString))
+						self.quotes.append("[%s] %s" % (user, tempString))
 						print("Quote has been added.")
 						self.sendMessage(self.channel, "Quote added.")
 					else:
@@ -28,12 +28,12 @@ class QuotesBot(UpdateBot):
 				elif (messageTokens[1].isnumeric()):
 					try:
 						quoteIndex = int(messageTokens[1])
-						if (quoteIndex > len(self.wedrs) or quoteIndex <= 0):
+						if (quoteIndex > len(self.quotes) or quoteIndex <= 0):
 							print("Index is invalid.")
 							self.sendMessage(self.channel, "Invalid quote index. Enter number from 1 to current size of Quotes List.")
 						else:
-							print("Showing quote #%s" % (str(self.wedrs[quoteIndex - 1])))
-							self.sendMessage(self.channel, "Quote #%s: %s" % (str(quoteIndex), str(self.wedrs[quoteIndex - 1])))
+							print("Showing quote #%s" % (str(self.quotes[quoteIndex - 1])))
+							self.sendMessage(self.channel, "Quote #%s: %s" % (str(quoteIndex), str(self.quotes[quoteIndex - 1])))
 					except ValueError:
 						print("Error converting string to integer.")
 					except Exception as error:
@@ -41,11 +41,11 @@ class QuotesBot(UpdateBot):
 				elif (messageTokens[1] == "remove" and len(messageTokens) > 2 and messageTokens[2].isnumeric()):
 					try:
 						quoteIndex = int(messageTokens[2])
-						if (quoteIndex > len(self.wedrs) or quoteIndex <= 0):
+						if (quoteIndex > len(self.quotes) or quoteIndex <= 0):
 							print("Index is invalid.")
 							self.sendMessage(self.channel, "Invalid quote index. Enter number from 1 to current size of Quotes List.")
 						else:
-							self.wedrs.remove(self.wedrs[quoteIndex - 1])
+							self.quotes.remove(self.quotes[quoteIndex - 1])
 							print("Successfully removed quote #%s" % (messageTokens[2]))
 							self.sendMessage(self.channel, "Quote #%s has been removed." % (messageTokens[2]))
 					except Exception:
@@ -53,7 +53,7 @@ class QuotesBot(UpdateBot):
 						self.sendMessage(self.channel, "Quote #%s does not exist." % (messageTokens[2]))
 				elif (messageTokens[1] == "capacity" or messageTokens[1] == "size"):
 					print("Showing quotes list size.")
-					self.sendMessage(self.channel, "Quotes List size : %s" % (str(len(self.wedrs))))
+					self.sendMessage(self.channel, "Quotes List size : %s" % (str(len(self.quotes))))
 				elif (messageTokens[1] == "help"):
 					self.sendMessage(self.channel, ".wedr add <MESSAGE> - Add MESSAGE to Quotes List.")
 					self.sendMessage(self.channel, ".wedr remove <INDEX> - Removes quote from Quotes List.")
