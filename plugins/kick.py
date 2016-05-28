56768fd0ea831ab034aae7704ddb1d6fb859d060
Thompson Lee <tom.mai78101@gmail.com>
2016-05-27 20:38:06 -0400

    Added Kick plugin

---
+++ plugins/kick.py
@@ -0,0 +1,13 @@
+import codecs
+from time import sleep
+from PluginBot import BYTE
+
+codecs.register(lambda name: codecs.lookup("utf-8") if name == "cp65001" else None)
+
+def version():
+	return "Kick - v1.0"
+
+def plugin_main(parent, tokens):
+	if (len(tokens) > 3):
+		if (tokens[1] == "KICK"):
+			parent.leave(tokens[2], True)
