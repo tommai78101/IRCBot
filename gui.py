c95a8c20730e866a448fddb87a11731c13426f6a
Thompson Lee <tom.mai78101@gmail.com>
2016-06-02 19:55:19 -0400

    A nice looking GUI using TKinter.

--- gui.py
+++ gui.py
@@ -1,21 +1,37 @@
-from tkinter import *
+import tkinter
+import tkinter.scrolledtext
 
-root = Tk()
+class GUI:
+	root = None
+	textOutput = None
 
-frame = Frame(root)
-frame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+	def __init__(self):
+		self.root = tkinter.Tk()
 
-top = Button(frame, text="Hello.", fg = "red")
-bottom = Button(frame, text="Hello.", fg = "blue")
-left = Button(frame, text="Hello.", fg = "green")
-right = Button(bottomFrame, text="Hello.", fg = "purple")
+		width = 450
+		height = 150
+		self.root.minsize(width, height)
 
-top.pack(side = LEFT)
-bottom.pack(side = LEFT)
-left.pack(side = LEFT)
-right.pack()
+		logMessageFrame = tkinter.Frame(master = self.root)
+		logMessageFrame.grid(row = 0, column = 0, sticky = (tkinter.N, tkinter.W, tkinter.E))
+		self.textOutput = tkinter.scrolledtext.ScrolledText(master = logMessageFrame, wrap = tkinter.WORD)
+		self.textOutput.pack(expand = True)
+
+		userInputFrame = tkinter.Frame(master = self.root, borderwidth = 5)
+		userInputFrame.grid(row = 1, column = 0, sticky = (tkinter.W, tkinter.E, tkinter.S))
+		tkinter.Button(master = userInputFrame, text = "Send").grid(row = 0, column = 0, sticky = (tkinter.W, tkinter.E), padx = 1.5)
+		tkinter.Entry(master = userInputFrame).grid(row = 0, column = 1, sticky = (tkinter.W, tkinter.E), padx = 1.5)
+
+		self.root.grid_rowconfigure(0, weight = 7)
+		self.root.grid_rowconfigure(1, weight = 1)
+		self.root.grid_columnconfigure(0, weight = 1)
+		userInputFrame.grid_rowconfigure(0, weight = 1)
+		userInputFrame.grid_columnconfigure(0, weight = 1)
+		userInputFrame.grid_columnconfigure(1, weight = 7)
 
 
-root.mainloop()
+	def run(self):
+		self.root.mainloop()
+		print("Hello")
+
+GUI().run()
