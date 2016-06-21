0e65fa511ffcf6bb886b1a3944148fa6c233c0f2
Thompson Lee <tom.mai78101@gmail.com>
2016-06-21 02:39:08 -0400

    Adding missing print functions for GUI.

--- plugins/rip.py
+++ plugins/rip.py
@@ -18,6 +18,8 @@ def plugin_main(parent, tokens):
 			if (len(tokens) > 4):
 				if (tokens[4] == "count"):
 					parent.s.send(PRIVMSG(tokens[2], "Total R.I.P. Count: %d" % ripObject.ripCount, 0))
+					if (parent.guiParent != None):
+						parent.guiParent.print("Total R.I.P. Count: %d" % ripObject.ripCount)
 				elif (tokens[4] == "help"):
 					parent.s.send(PRIVMSG(tokens[0], "USAGE: .rip - Gives the last spoken user an empathetic message.", 1))
 					parent.s.send(PRIVMSG(tokens[0], "USAGE: .rip [USER] - Gives the USER an empathetic message.", 1))
@@ -27,11 +29,15 @@ def plugin_main(parent, tokens):
 					ripObject.previousUser = tokens[4]
 					message = getMessage(tokens, 4)
 					parent.s.send(PRIVMSG(tokens[2], " -  R.I.P. %s   :(" % message, 0))
+					if (parent.guiParent != None):
+						parent.guiParent.print(" -  R.I.P. %s   :(" % message)
 					ripObject.ripCount += 1
 			else:
 				if (ripObject.previousUser != ""):
 					ripObject.ripCount += 1
-					parent.s.send(PRIVMSG(tokens[2], " -  R.I.P. %s   :(" % ripObject.previousUser, 0))			
+					parent.s.send(PRIVMSG(tokens[2], " -  R.I.P. %s   :(" % ripObject.previousUser, 0))
+					if (parent.guiParent != None):
+						parent.guiParent.print(" -  R.I.P. %s   :(" % ripObject.previousUser)
 		elif (tokens[3] == ".help"):
 			parent.s.send(PRIVMSG(tokens[0], "USAGE: .rip help - Show all .rip commands.", 1))
 		else:
